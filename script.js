9f985b77a9a8d9c85f9c395d3390ebb45b26610d
Caio Ribeiro <51218509+caiosribeiro@users.noreply.github.com>
2023-09-12 15:51:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,47 +10,55 @@ function digitar (){
         var n = Number(numero.value)
         switch(n){
             case 1:
-                saida.innerHTML = `Você digitou a <strong><u>opção ${n}</u></strong>, você acabou de doar <strong><u>R$10</u><strong>`
+                saida.innerHTML = `Você digitou a <strong><u>opção ${n}</u></strong>, </br> você acabou de doar <strong><u>R$10</u><strong>`
                 saida.style.fontSize = '20px'
-                saida.style.textAlign = 'right'
+                saida.style.textAlign = 'center'
                 saida.style.marginTop = '30px'
+                saida.style.color = 'yellow'
                 break
             case 2:
-                saida.innerHTML = `Você digitou a <strong><u>opção ${n}</u></strong>, você acabou de doar <strong><u>R$25</u></strong>`
+                saida.innerHTML = `Você digitou a <strong><u>opção ${n}</u></strong>, </br> você acabou de doar <strong><u>R$25</u></strong>`
                 saida.style.fontSize = '20px'
-                saida.style.textAlign = 'right'
+                saida.style.textAlign = 'center'
                 saida.style.marginTop = '30px'
+                saida.style.color = 'yellow'
                 break
             case 3:
-                saida.innerHTML = `Você digitou a <strong><u>opção ${n}</u></strong>, você acabou de doar <strong><u>R$50</u></strong>`
+                saida.innerHTML = `Você digitou a <strong><u>opção ${n}</u></strong>, </br> você acabou de doar <strong><u>R$50</u></strong>`
                 saida.style.fontSize = '20px'
-                saida.style.textAlign = 'right'
+                saida.style.textAlign = 'center'
                 saida.style.marginTop = '30px'
+                saida.style.color = 'yellow'
                 break
             case 4:
-                saida.innerHTML = `Você digiou a <strong><u>opção ${n}</u></strong>, Quanto você quer doar?`
+                saida.innerHTML = `Você digiou a <strong><u>opção ${n}</u></strong>, </br> Quanto você quer doar?`
                 saida.style.fontSize = '20px'
-                saida.style.textAlign = 'right'
+                saida.style.textAlign = 'center'
                 saida.style.marginTop = '30px'
                 debaixo.style.opacity = '1'
-                saida.style.float = 'center'
                 saida.style.padding = '10px'
-                saidan.style.padding = '10px'
-                saidan.style.float = 'right'
+                saidan.style.position = 'relative'
+                saidan.style.bottom = '50%'
+                saida.style.color = 'yellow'
                 
                 var a = Number(alt.value)
                 saidan.innerHTML = `Você acabou de <strong><u>doar R$${a}</u></strong>`
                 saidan.style.textTransform = 'uppercase'
                 saidan.style.textDecoration = 'underline'
+                saidan.style.color = 'yellow'
+                saidan.style.textAlign = 'right'
+                saidan.style.position = 'relative'
+                saidan.style.bottom = '50%'
+                saidan.style.padding = '10px'
                 break
 
             case 5:
-                saida.innerHTML = `Você digitou a <strong><u>opção ${n}</u></strong>, você acabou de doar <strong><u>R$0</u></strong>`
+                saida.innerHTML = `Você digitou a <strong><u>opção ${n}</u></strong>,</br> você acabou de doar <strong><u>R$0</u></strong>`
                 saida.style.fontSize = '20px'
-                saida.style.textAlign = 'right'
+                saida.style.textAlign = 'center'
                 saida.style.marginTop = '30px'
+                saida.style.color = 'yellow'
                 break
         }                
     }
 } 
-    
